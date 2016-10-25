5d5f560cf6a0fa0879b0e588f64a56d57e9cbec1
Lucas <lpzorzi@gmail.com>
2016-10-25 16:44:55 -0200

    adicionei email

--- Pessoas.java
+++ Pessoas.java
@@ -1,4 +1,13 @@
 public class Pessoas {
 
 	private String nome;
+	private String email;
+
+	public String getEmail(){
+		return email;
+	}
+
+	public void setEmail(String email){
+		this.email = email;
+	}
 }
